2e4b562e14b1f4ba329d51238e29de6003089b2b
Shloimy <shloimyelbaum@gmail.com>
2024-05-23 19:22:46 +0300

    Set up a simple user registration form that returns no errors for invalid input but when valid input is entered, the user is registered

--- YiddishVort/settings.py
+++ YiddishVort/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'users',
+    'articles',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +60,10 @@ ROOT_URLCONF = 'YiddishVort.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'users/templates'),
+            os.path.join(BASE_DIR, 'articles/templates'),
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +84,12 @@ WSGI_APPLICATION = 'YiddishVort.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'yiddishvort',
+        'USER': 'root',
+        'PASSWORD': 'Mymysql!12',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.views.generic import FormView
 # Create your views here.
+class UserRegistrationFormView(FormView):
+      template_name = 'users/user-auth.html'
+      form_class = UserCreationForm
+      success_url = '/login'
+      def form_valid(self, form):
+            form.save()
+            return super().form_valid(form)
+      def form_invalid(self, form):
+            return render(self.request, self.template_name, {'form': form, 'error': 'Invalid data'})
